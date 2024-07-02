f03116e75d5b373d1e5b4075cc0cfedc3a593db0
Robin Gerzaguet <45641052+RGerzaguet@users.noreply.github.com>
2024-07-02 11:59:35 +0200

    Update paths

--- install_tempest.sh
+++ install_tempest.sh
@@ -10,10 +10,10 @@ wget https://files.inria.fr/redinblack/assets/TempestSDRRuntime.zip
 
 echo "Unzipping the TempestSDR application..."
 unzip TempestSDRRuntime.zip
+mv ./TempestSDRRuntime/bin/dumpIQ_0.dat .
 
 echo "Launching the application and initializing the TempestSDR application... [This can take a while]"
 # Issue with MESA driver and Ubutu
 export LD_PRELOAD=/lib/x86_64-linux-gnu/libstdc++.so.6
 # Setup up everything
-cd TempestSDRRuntime
-./TempestSDRApp
+./TempestSDRRuntime/bin/TempestSDR
