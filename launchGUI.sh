7ee050e780937b1e87c99198b7a4dea9b8df50c8
Robin Gerzaguet <45641052+RGerzaguet@users.noreply.github.com>
2024-07-02 12:00:12 +0200

    Using bash CLI instead of julia configuration file

--- launchGUI.sh
+++ launchGUI.sh
@@ -4,4 +4,8 @@
 export LD_PRELOAD=/lib/x86_64-linux-gnu/libstdc++.so.6
 
 # Launch Julia
-./TempestSDR/bin/TempestSDR.app -L config.jl
+# With sdr=:radiosim, it uses a virtual radio to play with the GUI 
+./TempestSDR/bin/TempestSDR.app  sdr=:radiosim  addr=\"usb:1.7.5\" carrierFreq=764e6 samplingRate=20e6
+# With sdr=:pluto, it uses a real SDR devices
+# ./TempestSDR/bin/TempestSDR.app  sdr=:pluto  addr=\"usb:1.7.5\" carrierFreq=764e6
+
