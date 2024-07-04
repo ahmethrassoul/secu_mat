f66ee55812e5b1890aa5e47bfd4e69d5d3cd69ee
Robin Gerzaguet <robin.gerzaguet@enssat.fr>
2024-07-04 07:19:48 +0000

    By default, use the virtual radio

--- launchGUI.sh
+++ launchGUI.sh
@@ -5,7 +5,7 @@ export LD_PRELOAD=/lib/x86_64-linux-gnu/libstdc++.so.6
 
 # Launch Julia
 # With sdr=:radiosim, it uses a virtual radio to play with the GUI 
-#./TempestSDRRuntime//bin/TempestSDR  sdr=:radiosim  addr=\"usb:1.7.5\" carrierFreq=764e6 samplingRate=20e6
+./TempestSDRRuntime//bin/TempestSDR  sdr=:radiosim  addr=\"usb:1.7.5\" carrierFreq=764e6 samplingRate=20e6
 # With sdr=:pluto, it uses a real SDR devices
-./TempestSDRRuntime/bin/TempestSDR  sdr=:pluto  addr=\"ip:192.168.2.1\" carrierFreq=764e6 samplingRate=20e6
+#./TempestSDRRuntime/bin/TempestSDR  sdr=:pluto  addr=\"ip:192.168.2.1\" carrierFreq=764e6 samplingRate=20e6
 
